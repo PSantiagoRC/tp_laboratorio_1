2e1c815691d1fa0f4adec803a43d54387e1d393b
Sanchez_88 <chewiesan@gmail.com>
2022-04-08 21:52:05 -0300

    Segundo commit, correccion nombre carpeta con guion bajo

--- TP_1/src/TP_1.c
+++ TP_1/src/TP_1.c
@@ -54,10 +54,10 @@ int main(void) {
 		imprimeMenu();
 
 		//Ingreso de opción de menú
-		respuestaInt1 = getInt("\nSeleccione una opción del menú (opciones 1 a 6)",3,1,6,"\nError, ingrese número entre 1 y 6\n",&seleccion);
+		respuestaInt1 = getInt("\nSeleccione una opcion del menu (opciones 1 a 6)",3,1,6,"\nError, ingrese numero entre 1 y 6\n",&seleccion);
 		if(respuestaInt1 == 0)
 		{
-			printf("\n\nUd. ingresó la opción %d\n",seleccion);
+			printf("\n\nUd. ingreso la opcion %d\n",seleccion);
 			retorno = 0;
 		}
 		else
@@ -70,12 +70,21 @@ int main(void) {
 		switch(seleccion)
 		{
 			case 1:
-				respuestaFloat1 = getFloat("\nIngrese kilómetros de distancia de su viaje:\n",3,1,999999,"\nError, ingrese número entre 1 y 999999\n",&kmIngresados);
+				respuestaFloat1 = getFloat("\nIngrese kilometros de distancia de su viaje:\n",3,1,999999,"\nError, ingrese numero entre 1 y 999999\n",&kmIngresados);
 				if(respuestaFloat1 == 0)
 				{
-					printf("\nUd. ingresó %.2f kilómetros.\n",kmIngresados);
+					printf("\nUd. ingreso %.2f kilometros.\n",kmIngresados);
 					retorno = 0;
 					cargaInicial1 = 0;
+					printf("\n\t Aguarde para regresar al menu principal.");
+					Sleep(1000);
+					printf("\n\t3...");
+					Sleep(1000);
+					printf("\n\t2...");
+					Sleep(1000);
+					printf("\n\t1...");
+					Sleep(1000);
+					salida = 0;
 				}
 				else
 				{
@@ -86,22 +95,40 @@ int main(void) {
 				}
 				break;
 			case 2:
-				respuestaFloat2 = getFloat("\nIngrese precio del vuelo por Aerolineas:\n",3,1,999999,"\nError, ingrese número entre 1 y 999999\n",&precioIngresadoAerolineas);
+				respuestaFloat2 = getFloat("\nIngrese precio del vuelo por Aerolineas:\n",3,1,999999,"\nError, ingrese numero entre 1 y 999999\n",&precioIngresadoAerolineas);
 				if(respuestaFloat2 == 0)
 				{
-					printf("\nUd. ingresó un precio de %.2f por Aerolineas.\n",precioIngresadoAerolineas);
+					printf("\nUd. ingreso un precio de %.2f por Aerolineas.\n",precioIngresadoAerolineas);
 					cargaInicial2 = 0;
+					printf("\n\t Aguarde para regresar al menu principal.");
+					Sleep(1000);
+					printf("\n\t3...");
+					Sleep(1000);
+					printf("\n\t2...");
+					Sleep(1000);
+					printf("\n\t1...");
+					Sleep(1000);
+					salida = 0;
 				}
 				else
 				{
 					printf("\nERROR\n");
 				}
 
-				respuestaFloat3 = getFloat("\nIngrese precio del vuelo por Latam:\n",3,1,999999,"\nError, ingrese número entre 1 y 999999\n",&precioIngresadoLatam);
+				respuestaFloat3 = getFloat("\nIngrese precio del vuelo por Latam:\n",3,1,999999,"\nError, ingrese numero entre 1 y 999999\n",&precioIngresadoLatam);
 				if(respuestaFloat3 == 0)
 				{
-					printf("\nUd. ingresó un precio de %.2f por Latam.\n",precioIngresadoLatam);
+					printf("\nUd. ingreso un precio de %.2f por Latam.\n",precioIngresadoLatam);
 					cargaInicial3 = 0;
+					printf("\n\t Aguarde para regresar al menu principal.");
+					Sleep(1000);
+					printf("\n\t3...");
+					Sleep(1000);
+					printf("\n\t2...");
+					Sleep(1000);
+					printf("\n\t1...");
+					Sleep(1000);
+					salida = 0;
 				}
 				else
 				{
@@ -112,13 +139,22 @@ int main(void) {
 				if(cargaInicial1 == 0 && cargaInicial2 == 0 && cargaInicial3 == 0)
 				{
 					calculaCostos(kmIngresados, precioIngresadoLatam, precioIngresadoAerolineas, &costoDebitoLatam, &costoCreditoLatam, &costoDebitoAerolineas, &costoCreditoAerolineas, &costoBTCLatam, &costoBTCAerolineas, &precioPorKmLatam, &precioPorKmAerolineas, &diferenciaEmpresas);
-					printf("\nLos costos fueron calculados, ingrese opción 4 para visualizarlos.");
+					printf("\nLos costos fueron calculados, ingrese opcion 4 para visualizarlos.");
+					printf("\n\t Aguarde para regresar al menu principal.");
+					Sleep(1000);
+					printf("\n\t3...");
+					Sleep(1000);
+					printf("\n\t2...");
+					Sleep(1000);
+					printf("\n\t1...");
+					Sleep(1000);
+					salida = 0;
 					cargaInicial4 = 0;
 				}
 				else
 				{
-					printf("\n\nError: debe cargar la distancia en kilómetros (opción 1) y ambos precios (opción2) para calcular costos.\n");
-					printf("\n\t Aguarde para regresar al menú principal.");
+					printf("\n\nError: debe cargar la distancia en kilometros (opcion 1) y ambos precios (opcion2) para calcular costos.\n");
+					printf("\n\t Aguarde para regresar al menu principal.");
 					Sleep(3000);
 					printf("\n\t3...");
 					Sleep(2000);
@@ -135,8 +171,8 @@ int main(void) {
 				}
 				else
 				{
-					printf("\n\nDebe cargar la distancia en kilómetros (opcion 1), ambos precios (opcion 2) y calcular costos (opcion 3) para seleccionar esta opción.\n");
-					printf("\n\t Aguarde para regresar al menú principal.");
+					printf("\n\nDebe cargar la distancia en kilometros (opcion 1), ambos precios (opcion 2) y calcular costos (opcion 3) para seleccionar esta opcion.\n");
+					printf("\n\t Aguarde para regresar al menu principal.");
 					Sleep(3000);
 					printf("\n\t3...");
 					Sleep(2000);
@@ -150,11 +186,28 @@ int main(void) {
 				kmIngresados = KM;
 				precioIngresadoLatam = PL;
 				precioIngresadoAerolineas = PA;
+
+				printf("\nCarga forzada:\n");
+
 				calculaCostos(kmIngresados, precioIngresadoLatam, precioIngresadoAerolineas, &costoDebitoLatam, &costoCreditoLatam, &costoDebitoAerolineas, &costoCreditoAerolineas, &costoBTCLatam, &costoBTCAerolineas, &precioPorKmLatam, &precioPorKmAerolineas, &diferenciaEmpresas);
 				imprimeCostos(costoDebitoLatam, costoDebitoAerolineas, costoCreditoLatam, costoCreditoAerolineas, costoBTCLatam, costoBTCAerolineas, precioPorKmLatam, precioPorKmAerolineas, diferenciaEmpresas);
+				printf("\n\t Aguarde para regresar al menu principal.");
+				Sleep(3000);
+				printf("\n\t3...");
+				Sleep(2000);
+				printf("\n\t2...");
+				Sleep(2000);
+				printf("\n\t1...");
+				Sleep(2000);
 				break;
 			default:
 				printf("\nUsted ha salido del aplicativo.");
+				printf("\n\t3...");
+				Sleep(1000);
+				printf("\n\t2...");
+				Sleep(1000);
+				printf("\n\t1...");
+				Sleep(1000);
 				salida = 0;
 				break;
 		}
--- TP_1/src/auxiliar.c
+++ TP_1/src/auxiliar.c
@@ -31,13 +31,13 @@ void calculaCostos(float kmIngresados,float precioIngresadoLatam,float precioIng
 void imprimeCostos(float costoDebitoLatam, float costoDebitoAerolineas, float costoCreditoLatam, float costoCreditoAerolineas, float costoBTCLatam, float costoBTCAerolineas, float precioPorKmLatam, float precioPorKmAerolineas, float diferenciaEmpresas)
 {
 	printf("\nResultados:\nLatam:");
-	printf("\na)Precio con tarjeta de débito: %.2f",costoDebitoLatam);
-	printf("\nb)Precio con tarjeta de crédito: %.2f",costoCreditoLatam);
+	printf("\na)Precio con tarjeta de debito: %.2f",costoDebitoLatam);
+	printf("\nb)Precio con tarjeta de credito: %.2f",costoCreditoLatam);
 	printf("\nc)Precio pagando con bitcoin: %.5f",costoBTCLatam);
 	printf("\nd)Precio unitario: %.2f",precioPorKmLatam);
-	printf("\nAerolíneas:");
-	printf("\na)Precio con tarjeta de débito: %.2f",costoDebitoAerolineas);
-	printf("\nb)Precio con tarjeta de crédito: %.2f",costoCreditoAerolineas);
+	printf("\nAerolineas:");
+	printf("\na)Precio con tarjeta de debito: %.2f",costoDebitoAerolineas);
+	printf("\nb)Precio con tarjeta de credito: %.2f",costoCreditoAerolineas);
 	printf("\nc)Precio pagando con bitcoin: %.5f",costoBTCAerolineas);
 	printf("\nd)Precio unitario: %.2f",precioPorKmAerolineas);
 	printf("\nLa diferencia de precio es : %.2f\n",diferenciaEmpresas);
@@ -45,25 +45,25 @@ void imprimeCostos(float costoDebitoLatam, float costoDebitoAerolineas, float co
 
 void imprimeMenu(void)
 {
-	printf("\n\n1. Ingresar Kilómetros: ( km=x)");
-	printf("\n\n2. Ingresar Precio de Vuelos: (Aerolíneas=y, Latam=z)");
-	printf("\n\t- Precio vuelo Aerolíneas:");
+	printf("\n\n1. Ingresar Kilometros: ( km=x)");
+	printf("\n\n2. Ingresar Precio de Vuelos: (Aerolineas=y, Latam=z)");
+	printf("\n\t- Precio vuelo Aerolineas:");
 	printf("\n\t- Precio vuelo Latam:");
 	printf("\n\n3. Calcular todos los costos:");
-	printf("\na) Tarjeta de débito (descuento 10%%)");
-	printf("\nb) Tarjeta de crédito (interés 25%%)");
+	printf("\na) Tarjeta de debito (descuento 10%%)");
+	printf("\nb) Tarjeta de credito (interes 25%%)");
 	printf("\nc) Bitcoin (1BTC -> 4606954.55 Pesos Argentinos)");
 	printf("\nd) Mostrar precio por km (precio unitario)");
-	printf("\ne) Mostrar diferencia de precio ingresada (Latam - Aerolíneas)");
+	printf("\ne) Mostrar diferencia de precio ingresada (Latam - Aerolineas)");
 	printf("\n\n4. Informar Resultados");
 	printf("\nLatam:");
-	printf("\na) Precio con tarjeta de débito: r");
-	printf("\nb) Precio con tarjeta de crédito: r");
+	printf("\na) Precio con tarjeta de debito: r");
+	printf("\nb) Precio con tarjeta de credito: r");
 	printf("\nc) Precio pagando con bitcoin : r");
 	printf("\nd) Precio unitario: r");
-	printf("\nAerolíneas:");
-	printf("\na) Precio con tarjeta de débito: r");
-	printf("\nb) Precio con tarjeta de crédito: r");
+	printf("\nAerolineas:");
+	printf("\na) Precio con tarjeta de debito: r");
+	printf("\nb) Precio con tarjeta de credito: r");
 	printf("\nc) Precio pagando con bitcoin : r");
 	printf("\nd) Precio unitario: r ");
 	printf("\nLa diferencia de precio es : r");
